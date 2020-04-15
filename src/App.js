aa71734859c933465d38418b9159c0a6fdddd6ad
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-04-14 18:36:10 -0700

    type: Adding assets
    body: Added assets for all components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello</p>
       </header>
+      <body className="App-body">
+        <h1>Want to Read</h1>
+        <h1>Reading</h1>
+        <h1>Read</h1>
+      </body>
     </div>
   );
 }
